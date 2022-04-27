4fa01604da67e2d0990014df92dc679e7b182931
Akanksha1212 <singhakanksha221b@gmail.com>
2022-04-27 22:10:49 +0530

    OneSignal Flutter App

---
+++ README.md
@@ -0,0 +1,12 @@
+# Push Notification Integration in Flutter
+
+Notes:
+
+- Create a FREE [OneSignal](https://onesignal.com/) account  
+- In home.dart add OneSignalApp Id.
+
+## Getting Started
+
+For help getting started with Flutter, view
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  static final String oneSignalAppId = "********App Id Goes Here****";
+  Future<void> initPlatformState() async {
+    OneSignal.shared.setAppId(oneSignalAppId);
+    OneSignal.shared
+        .promptUserForPushNotificationPermission()
+        .then((accepted) {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text("Hello"),
+          ],
+        ),
+      ),
+    );
+  }
+}
